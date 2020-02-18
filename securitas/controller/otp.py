4eafd63648e10409a42fea222dd65446f32fce90
Rick Elrod <rick@elrod.me>
2020-02-17 21:34:23 -0600

    Handle OTP adds
    Signed-off-by: Rick Elrod <rick@elrod.me>

--- securitas/controller/otp.py
+++ securitas/controller/otp.py
@@ -1,5 +1,6 @@
 from flask import flash, redirect, render_template, session, url_for
 import python_freeipa
+import uuid
 
 from securitas import app
 from securitas.form.otp import OTPForm
@@ -16,7 +17,10 @@ def otp_listing(ipa):
             [],
             {'ipatokenowner': username})['result']
     ]
-    return render_template('otp/list.html', tokens=resp)
+    otp_uri = session.get('otp_uri')
+    # Reset this so we only show it once.
+    session['otp_uri'] = None
+    return render_template('otp/list.html', tokens=resp, otp_uri=otp_uri)
 
 @app.route('/otp/delete/<tokenid>/')
 @with_ipa(app, session)
@@ -28,6 +32,7 @@ def otp_delete(ipa, tokenid):
             {'ipatokenuniqueid': tokenid})
         flash('Deleted the token.', 'success')
     except:
+        # TODO: Error handling
         flash('Could not delete the token.', 'danger')
     return redirect(url_for('otp_listing'))
 
@@ -36,6 +41,21 @@ def otp_delete(ipa, tokenid):
 def otp_add(ipa):
     form = OTPForm()
     if form.validate_on_submit():
-        flash('W00T', 'success')
+        username = session.get('securitas_username')
+        token_id = uuid.uuid4()
+        description = form.description.data
+        try:
+            resp = ipa._request(
+                'otptoken_add',
+                [],
+                { 'ipatokenowner': username,
+                  'ipatokenotpalgorithm': 'sha512',
+                  'description': description,
+                })
+            session['otp_uri'] = resp['result']['uri']
+        except Exception as e:
+            # TODO: Error handling
+            flash('Could not add the token.', 'danger')
+            flash(e)
         return redirect(url_for('otp_listing'))
     return render_template('otp/add.html', form=form)
